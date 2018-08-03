8f0a75d51294e60fc547d6f0f8112568f1ed2b2c
sreym <sergeyreym@gmail.com>
2018-08-04 01:24:08 +0800

    fix indent

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,42 @@
 import * as THREE from 'three';
 
-let scene:THREE.Scene;
-let camera:THREE.PerspectiveCamera;
-let renderer:THREE.WebGLRenderer;
-let mesh:THREE.Mesh;
+let scene: THREE.Scene;
+let camera: THREE.PerspectiveCamera;
+let renderer: THREE.WebGLRenderer;
+let mesh: THREE.Mesh;
 
-function init():void {
-  camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
+function init(): void {
+  camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
   camera.position.z = 400;
   scene = new THREE.Scene();
-  let texture = new THREE.TextureLoader().load( 'textures/crate.gif' );
-  let geometry = new THREE.BoxBufferGeometry( 200, 200, 200 );
-  let material = new THREE.MeshBasicMaterial( { map: texture } );
-  mesh = new THREE.Mesh( geometry, material );
-  scene.add( mesh );
-  renderer = new THREE.WebGLRenderer( { antialias: true } );
-  renderer.setPixelRatio( window.devicePixelRatio );
-  renderer.setSize( window.innerWidth, window.innerHeight );
+  let texture = new THREE.TextureLoader().load('textures/crate.gif');
+  let geometry = new THREE.BoxBufferGeometry(200, 200, 200);
+  let material = new THREE.MeshBasicMaterial({ map: texture });
+  mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
-  document.body.appendChild( renderer.domElement );
-  window.addEventListener( 'resize', onWindowResize, false );
+  document.body.appendChild(renderer.domElement);
+  window.addEventListener('resize', onWindowResize, false);
 }
 
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
-  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 function animate(): void {
   requestAnimationFrame(animate);
 
-  let timer = 0.002*Date.now();
+  let timer = 0.002 * Date.now();
 
-	mesh.position.y = 0.5 + 0.5*Math.sin(timer);
+  mesh.position.y = 0.5 + 0.5 * Math.sin(timer);
   mesh.rotation.x += 0.03;
-  
-	renderer.render(scene, camera);
+
+  renderer.render(scene, camera);
 }
 
 init();
