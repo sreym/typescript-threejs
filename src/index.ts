6356dd625d89608d875a905a950943971315bd16
sreym <sergeyreym@gmail.com>
2018-08-11 12:35:36 +0800

    add fbxlaoder and sample model

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import * as THREE from 'three';
+import * as initFBXLoader from './fbxloader.js';
+initFBXLoader(THREE);
 
 let scene: THREE.Scene;
 let camera: THREE.PerspectiveCamera;
 let renderer: THREE.WebGLRenderer;
 let mesh: THREE.Mesh;
 let raycaster: THREE.Raycaster;
+let clock = new THREE.Clock();
 
 let mouse = new THREE.Vector2();
 
 let light1:THREE.PointLight;
+
+let mixers:any[] = [];
 
 function init(): void {
   scene = new THREE.Scene();
@@ -21,7 +26,7 @@ function init(): void {
   // let material = new THREE.MeshBasicMaterial({ map: texture });
   let material = new THREE.MeshLambertMaterial( { color: 0x2194CE } );
   mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
+  // scene.add(mesh);
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
@@ -29,6 +34,21 @@ function init(): void {
   light1 = new THREE.PointLight( 0xff0040, 10, 2000 );
   scene.add( light1 );
   light1.position.set(100, 0, 300);
+
+  var loader = new THREE.FBXLoader();
+  loader.load( 'Samba Dancing.fbx', function ( object:any ) {
+    object.mixer = new THREE.AnimationMixer( object );
+    mixers.push( object.mixer );
+    var action = object.mixer.clipAction( object.animations[ 0 ] );
+    action.play();
+    object.traverse( function ( child:any ) {
+      if ( child.isMesh ) {
+        child.castShadow = true;
+        child.receiveShadow = true;
+      }
+    } );
+    scene.add( object );
+  } );
 
   document.body.appendChild(renderer.domElement);
   window.addEventListener('resize', onWindowResize, false);
@@ -68,6 +88,11 @@ function animate(): void {
     intersects[i].object.scale.x *= 1.001;
   }
 
+  if ( mixers.length > 0 ) {
+    for ( var i = 0; i < mixers.length; i ++ ) {
+      mixers[ i ].update( clock.getDelta() );
+    }
+  }
   renderer.render(scene, camera);
 }
 
